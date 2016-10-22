889aaf7fb9819008939c56fa1d4b521e57060a7b
Ben <bcootner@gmail.com>
2016-10-22 07:31:46 -0400

    scrolly web-view and table view

---
+++ ResumeQ/companyCell.swift
@@ -0,0 +1,24 @@
+//
+//  companyCell.swift
+//  ResumeQ
+//
+//  Created by Ben Cootner on 10/22/16.
+//  Copyright © 2016 Ben Cootner. All rights reserved.
+//
+
+import UIKit
+
+class companyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
